30717b8665e9e3e037d3c9297643351f707a5ced
sskimin4 <37132420+sskimin4@users.noreply.github.com>
2019-08-13 01:11:02 +0900

    Add files via upload

---
+++ [Project]/common/puzzle_dev.h
@@ -0,0 +1,17 @@
+#ifndef __PUZZLE_DEV__
+#define __PUZZLE_DEV__
+
+#define PUZZLE_MAJOR 242
+#define PUZZLE_MINOR 0
+#define PUZZLE_NAME "puzzle_dev"
+
+
+
+/* ioctl macros */
+#define PUZZLE_PUT  _IOW(PUZZLE_MAJOR, 1, struct __puzzle_data*)
+#define PUZZLE_DATA_ENABLE  _IOW(PUZZLE_MAJOR, 2, int*)
+#define PUZZLE_GET  _IOR(PUZZLE_MAJOR, 3, struct __puzzle_data*)
+#define PUZZLE_GET_ENABLE  _IOR(PUZZLE_MAJOR, 4, int*)
+#define PUZZLE_CHANGE  _IOW(PUZZLE_MAJOR,5,struct __puzzle_data*)
+#define PUZZLE_CHANGE_ENABLE  _IOR(PUZZLE_MAJOR,6,int *)
+#endif /* __PUZZLE_DEV__ */
